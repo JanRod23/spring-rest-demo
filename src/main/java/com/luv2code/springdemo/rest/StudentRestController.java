ec02bc2242037e5990d1a6de4cea87c3d49f3e7b
Jansen Rodriguez <JansenR125@gmail.com>
2021-02-07 16:58:11 -0500

    Modified student list creation to class scope

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +15,22 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// Define endpoint for "/students" - return list of all students
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	private List<Student> theStudents;
+	
+	// Define @PostConstruct to load the student data ONLY ONCE
+	@PostConstruct
+	public void loadData() {
 		
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Tom","Brady"));
 		theStudents.add(new Student("Patrick","Mahomes"));
 		theStudents.add(new Student("Lamar","Jackson"));
-		
+	}
+	
+	// Define endpoint for "/students" - return list of all students
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		return theStudents;
 	}
 }
